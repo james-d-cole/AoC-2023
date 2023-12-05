0224ef418d9f043950a01a30dce5558d994d1dc2
James Cole <james.cole@specsavers.com>
2023-12-05 14:56:09 +0000

    day 3 complete

--- day_3_gear_ratios/solution_updated.py
+++ day_3_gear_ratios/solution_updated.py
@@ -1,5 +1,6 @@
 # Import required packages
 import re
+import numpy
 
 # Read input
 with open('input.txt') as source_file:
@@ -15,7 +16,6 @@ def symbol_search(num, row, col):
                 if (len(items) > cr >= 0 and len(items[0]) > cc >= 0) and not bool(re.search(r'\d|\.', items[cr][cc])):
                     return int(num)
     return 0
-
 
 # Part 1
 part_sum = 0
@@ -33,7 +33,42 @@ for row in range(len(items)):
             num_str = ''
 print(f"Answer to question 1: {part_sum}")
 
-# Part 2
+# Part 2 - functions
+def symbol_search(num, row, col):
+    if len(num) > 0:
+        for y in [-1, 0, 1]:
+            for x in range(len(num)+2):
+                cr, cc = row+y, col-x
+                if (len(items) > cr >= 0 and len(items[0]) > cc >= 0) and bool(re.search(r'\*', items[cr][cc])):
+                    matrix[cr][cc] = matrix[cr][cc] + [int(num)]
+    return 0
 
+# Part 1
+matrix = []
+for i in range(len(items)):
+    row = [[1]]*len(items[0])
+    matrix.append(row)
+gear_sum = 0
+num_str = ''
+for row in range(len(items)):
+    for col in range(len(items[row])):
+        if items[row][col].isdigit() and col < len(items[row])-1:
+            num_str += items[row][col]
+        elif items[row][col].isdigit() and col == len(items[row])-1:
+            num_str += items[row][col]
+            gear_sum += symbol_search(num_str, row, col)
+            num_str = ''
+        else:
+            gear_sum += symbol_search(num_str, row, col)
+            num_str = ''
+for row in range(len(matrix)):
+    for col in range(len(matrix[row])):
+        if len(matrix[row][col])==3:
+            gear_sum += numpy.prod(matrix[row][col])
+
+
+
+
+print(f"Answer to question 2: {gear_sum}")
 
 # 527446 is right
